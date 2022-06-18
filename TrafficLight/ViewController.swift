973d151a83e11bd6bb1b46a8f84090b85f513a75
ilyakozhevn <kozhevilya@gmail.com>
2022-06-18 20:35:49 +0300

    1. Interface using Stack 2. Logic within main view

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lights: [UIView]!
+    @IBOutlet var selectorButton: UIButton!
+    
+    private var colorSelectorPosition = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fadeAll()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        roundCorners()
     }
 
-
+    @IBAction func pushSelectorButton() {
+        selectorButton.setTitle("Next", for: .normal)
+        fadeAll()
+        lights[colorSelectorPosition].alpha = 1
+        turnSelector()
+    }
+    
+    private func fadeAll() {
+        for light in lights {
+            light.alpha = 0.1
+        }
+    }
+    
+    private func roundCorners() {
+        for light in lights {
+            light.layer.masksToBounds = true
+            light.layer.cornerRadius = light.layer.frame.height * 0.5
+        }
+        selectorButton.layer.cornerRadius = 5
+    }
+    
+    private func turnSelector() {
+        colorSelectorPosition += 1
+        if colorSelectorPosition == lights.count {colorSelectorPosition = 0}
+    }
 }
 
